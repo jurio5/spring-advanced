e14e4fbdce0225d063ea37eff8c2fc70f7604326
jurio5 <sia5492@naver.com>
2025-07-22 03:13:25 +0900

    [feat] 파라미터 메서드로 넘겨서 로그 동기화 맞추는 작업

--- src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
+++ src/test/java/hello/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -1,16 +1,11 @@
-package hello.advanced.app.hellotrace;
+package hello.advanced.trace.hellotrace;
 
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV1;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloTraceV1Test {
-
-    @Autowired
-    HelloTraceV1 helloTraceV1;
 
     @Test
     void begin_end() {
