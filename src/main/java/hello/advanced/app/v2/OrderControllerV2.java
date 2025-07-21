e8fb21a526282d8a2fe633ed804452a5932ac1f4
jurio5 <sia5492@naver.com>
2025-07-22 03:44:40 +0900

    [feat] 로그 동기화를 맞춰서 레벨까지 표시

---
+++ src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,32 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v2")
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져줘야 함
+        }
+    }
+}
