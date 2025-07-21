09d92b4b8a02ba8d1b70a61b85c4009a7053f8c2
jurio5 <sia5492@naver.com>
2025-07-22 00:49:30 +0900

    [feat] 로그 추적기 V0

---
+++ src/test/java/hello/advanced/app/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.hellotrace;
+
+import hello.advanced.app.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloTraceV1Test {
+
+    @Autowired
+    HelloTraceV1 helloTraceV1;
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+
+}
